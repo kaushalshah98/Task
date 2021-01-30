d822ab3d3737aa5afa3dd67470dbd21563c64eed
kaushalshah589@gmail.com <kaushal.shah@actonate.com>
2021-01-30 23:39:20 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,25 @@ import '@tailwindcss/ui/dist/tailwind-ui.css';
 import React, { useState } from 'react';
 import ReactNotification from 'react-notifications-component';
 import 'react-pro-sidebar/dist/css/styles.css';
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Authentication from './components/Auth/Authentication';
 import { Login } from './components/Auth/Login';
 import Registration from './components/Auth/Registration';
 import Dashboard from './components/Dashboard/Dashboard';
 import './css/fonts.css';
 import './css/style.css';
-
 const App = (props: any) => {
   let [isAuth, setIsAuth] = useState(false);
-
   return (
     <>
       <ReactNotification />
       <Router>
-        {/*  <Provider /> */}
-
+        <Route exact path="/">
+          <Registration />
+        </Route>
         <Route exact path="/login">
           <Login isAuth={isAuth} setIsAuth={setIsAuth} />
         </Route>
-        <Redirect exact from="/" to="dashboard" />
         <Route exact path="/registration">
           <Registration />
         </Route>
@@ -31,5 +29,4 @@ const App = (props: any) => {
     </>
   );
 };
-
 export default App;
--- src/config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  BUCKETNAME: 'http://s3.ap-southeast-1.amazonaws.com/actonate-store/actiondesk/',
-  // BASE_URL: "https://localhost:5001/",
-  // BASE_URL: "http://api.testdesk.apps.actonatepanel.com/"
-  BASE_URL: 'http://api-stage.testdesk.apps.actonatepanel.com/'
-};
-
-export { config };
